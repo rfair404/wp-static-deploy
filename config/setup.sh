c6d85fc17410c0d7de099e1a56216a896499a7fd
russell.fair <russell.fair@10up.com>
2019-11-14 11:20:17 -0700

    try setup again

--- config/setup.sh
+++ config/setup.sh
@@ -4,7 +4,7 @@ set -o allexport
 set +o allexport
 
 install_wp() {
-    wp config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASS --dbhost=$WP_DB_HOST --allow-root
+#    wp config create --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASS --dbhost=$WP_DB_HOST --allow-root
     wp db reset --allow-root --yes
     wp core install --url=$WP_URL --title="$WP_TITLE" --admin_user=$ADMIN_USER --admin_email=admin@example.com --admin_password=password --skip-email --allow-root
     RFAIR_USER=$(wp user create rfair404 rfair404@gmail.com --role=author --first_name="Russell" --last_name="Fair" --porcelain --allow-root)
@@ -12,7 +12,8 @@ install_wp() {
 }
 
 activate_plugins() {
- wp plugin activate static-html-output-plugin --allow-root
+    wp plugin activate static-html-output-plugin --allow-root
+#    wp plugin activate wp-githuber-md --allow-root
 }
 
 delete_posts() {
@@ -27,10 +28,25 @@ create_homepage() {
 }
 
 configure_wp2static() {
-    wp option update wp2static-options $(wp eval-file ./config/wp2static.php --allow-root) --allow-root
+#    wp option update wp2static-options $(wp eval-file ./config/wp2static.php --allow-root) --allow-root
+    wp wp2static options set targetFolder /var/www/html/static-cli --allow-root
+    wp wp2static options set selected_deployment_option github --allow-root
+    wp wp2static options set baseUrl https://rfair404.github.io/wp-static-deploy --allow-root
+    wp wp2static options set baseUrl-github https://rfair404.github.io/wp-static-deploy --allow-root
+    wp wp2static options set ghBranch gh-pages --allow-root
+    wp wp2static options set ghToken $GH_TOKEN --allow-root
+    wp wp2static options set ghRepo rfair404/wp-static-deploy --allow-root
+    wp wp2static options set ghCommitMessage "site update" --allow-root
 }
-install_wp
-activate_plugins
-delete_posts
-create_homepage
+
+generate_wp2static() {
+    wp wp2static generate --allow-root
+    wp wp2static deploy --allow-root
+}
+
+#install_wp
+#activate_plugins
+#delete_posts
+#create_homepage
 configure_wp2static
+generate_wp2static
